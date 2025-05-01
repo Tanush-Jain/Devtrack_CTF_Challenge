6fd39ca08b34962344f0c5a3b3b51c06c33cf6c7
Tanush-Jain <tanushjain2004@hotmail.com>
2025-05-01 17:24:01 +0530

    Update requirements.txt with essential packages

--- ctf_app/wsgi.py
+++ ctf_app/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for ctf_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctf_app.settings')
+
+import django
+from django.core.management import call_command
+
+django.setup()
+call_command('migrate', interactive=False)
 
 application = get_wsgi_application()
